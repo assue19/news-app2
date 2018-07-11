cb55190d8a56551f52fcbb304b4516929438dc25
assumpter kimeu <assumpterkimeu16@gmail.com>
2018-07-11 15:06:06 +0300

    initial repo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: 'My App'
+    }
+  }
+}
+</script>
